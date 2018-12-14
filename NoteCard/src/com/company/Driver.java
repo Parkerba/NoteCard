b9d5527d0fe6fc555e662a0b3915a90273173b68
Parker Amundsen <parkera13@gmail.com>
2018-12-14 15:51:47 -0800

    First version of NoteCard class

--- NoteCard/src/com/company/Driver.java
+++ NoteCard/src/com/company/Driver.java
@@ -3,5 +3,11 @@ package com.company;
 public class Driver {
 
     public static void main(String[] args) {
+        NoteCard melon = new NoteCard("How old is Melon turning?", "20");
+
+        System.out.println(melon.getQuestion());
+        System.out.println(melon.getAnswer());
+
+
     }
 }
--- NoteCard/src/com/company/NoteCard.java
+++ NoteCard/src/com/company/NoteCard.java
@@ -1,4 +1,44 @@
 package com.company;
 
 public class NoteCard {
+    /**
+     * @descrip String data members question and answer encapsulate the two sides of a notecard.
+     */
+    private String question;
+    private String answer;
+
+    /**
+     * @descrip NoteCard obj constructor, takes two String parameters.
+     * @param question
+     * @param answer
+     */
+    public NoteCard(String question, String answer){
+        this.answer = answer;
+        this.question = question;
+    }
+
+    /**
+     * @descrip getter for String data member question.
+     * @return question
+     */
+    public String getQuestion(){
+        return this.question;
+    }
+    /**
+     * @descrip getter for String data member answer.
+     * @return answer
+     */
+    public String getAnswer(){
+        return this.answer;
+    }
+
+
+    /**
+     * @descrip toString() override.
+     * @return returns formatted NoteCard obj as String.
+     */
+    @Override
+    public String toString(){
+       return "Q: " +  this.question + "\n" + "A: " + this.answer + "\n";
+    }
 }
